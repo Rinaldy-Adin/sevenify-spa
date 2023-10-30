7a6a37292809476d8379d8b768582cb7611fff1a
Husnia Munzayana <13521077@std.stei.itb.ac.id>
2023-10-30 23:45:53 +0700

    dev: add responsive page

--- src/pages/Join/RequestJoin.jsx
+++ src/pages/Join/RequestJoin.jsx
@@ -20,17 +20,18 @@ export default function RequestJoin() {
 
   return (
 <div className="frame-join flex flex-col items-center gap-2.5 w-full bg-white">
-<div className="join-background flex flex-col items-end gap-2.5 self-stretch py-11 px-16 h-[400px] text-white font-['Plus text-[3.375rem] font-bold leading-[normal]']"
+<div className="join-background flex flex-col justify-between items-start gap-2.5 self-stretch py-11 px-16 h-[400px]"
   style={backgroundStyle}>
     {isJoined ? (
-      <>
+      <h1 className="text-white text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold leading-[normal]']">
         Hold on tight, your Sevenify+ request is being reviewed.
-      </>
+      </h1>
     ) : (
       <>
-        Share music with us, join Sevenify+
-        <button
-          className= "btn btn-primary text-lg font-bold px-3 py-2" 
+        <h1 className="text-white text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold leading-[normal]']">
+          Share music with us, join Sevenify+
+        </h1><button
+          className= "btn btn-primary text-lg font-bold px-3 py- self-end" 
           onClick={handleJoin}
         >
         Join Sevenify+
@@ -41,15 +42,15 @@ export default function RequestJoin() {
   </div>
   <div className="flex flex-col items-start gap-2.5 self-stretch py-6 px-0">
     <div className="flex flex-col items-start gap-2.5 self-stretch p-2.5 p-2">
-      <div className="Jakarta Sans'] self-stretch text-black font-['Plus text-4xl font-bold leading-[normal]">
+      <div className="Jakarta Sans self-stretch text-black font-['Plus text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl font-bold leading-[normal]']">
         Why join Sevenify+?
       </div>
-      <div className="Jakarta Sans'] self-stretch text-black font-['Plus text-lg leading-[normal]">
+      <div className="Jakarta Sans self-stretch text-black font-['Plus text-1rem sm:text-1rem md:text-xl lg:text-xl xl:text-xl leading-[normal]']">
         Sevenify+ is a premium subscription service that offers a wide range of exclusive features designed to enhance your experience. With a Sevenify+ subscription, you gain access to premium music content, unlocking a world of high-quality tracks and personalized playlists that cater to your unique musical preferences. Our commitment to delivering exceptional music experiences ensures that you can enjoy your favorite songs with superior audio quality. But Sevenify+ doesn't stop there; it also provides other exclusive benefits that make your subscription worthwhile, ensuring you have access to a holistic package of entertainment and convenience. Join Sevenify+ today and elevate your music enjoyment to new heights.
       </div>
     </div>
     <div className="flex flex-col items-start gap-4 self-stretch p-2.5 p-2">
-      <div className="Jakarta Sans'] self-stretch text-black font-['Plus text-4xl font-bold leading-[normal]">
+      <div className="Jakarta Sans self-stretch text-black font-['Plus text-2xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-4xl  font-bold leading-[normal]']">
         Log in with a different account
       </div>
       <button className="btn btn-primary text-lg font-bold px-3 py-2" onClick={handleLogout}>
